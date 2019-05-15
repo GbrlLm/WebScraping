0ebe2a7e8089807a72b24a71b6c4471aacebbf02
gabriel <gabriel.lima@modalgr.com.br>
2019-05-15 16:54:12 -0300

    implementação da requisição

--- js.js
+++ js.js
@@ -1,38 +1,39 @@
 function montarPagina(pagina){
     var parser = new DOMParser();
     var documento = parser.parseFromString(pagina, "text/html");
-    console.log(documento);
-    // documento.querySelectorAll(".noticias-direita").forEach(function(div){
-    //     var as = div.children;
-    //     Array.from(as).forEach(function(a){
-    //         var noticia = a.children;
-    //         Array.from(noticia).forEach(x => document.body.appendChild(x));
-    //     })
-        //var div2 = document.createElement("div");
-        //div2.innerHTML = p;
-        //document.body.appendChild(div2);
-    // });
+    documento.querySelectorAll(".slick-slide").forEach(div => {
+        var li = div.children;
+        // Array.from(as).forEach(function(a){
+        //     var noticia = a.children;
+        //     Array.from(noticia).forEach(x => document.body.appendChild(x));
+        // });
+        console.log(li);
+        // var div2 = document.createElement("div");
+        // div2.innerHTML = p;
+        // document.body.appendChild(div2);
+    });
 }
 
-//description-noticia
-function sendReq() {
+//Nerdonautas
+function NerdonautasReq() {
     var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://www.nerdonautas.com.br/", true);
+    
     xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        montarPagina(this.responseText);
-        console.log("OK");
-      }
-      else{
-          console.log("erro");
-      }
+        if (this.readyState == 4 && this.status == 200) {
+            montarPagina(this.responseText);
+            console.log("OK");
+        }
+        else{
+            console.log("erro");
+        }
     };
-  
-  xhttp.open("GET", "https://www.nerdonautas.com.br/", true);
-  xhttp.send();
+    
+    xhttp.send();
 }
 
 function teste(){
-    sendReq();
+    NerdonautasReq();
 }
 
 function criarEvento(){
