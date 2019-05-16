abe81aad174b785a0d2e6367162513f4d6274c3c
gabriel <gabriel.lima@modalgr.com.br>
2019-05-16 09:10:57 -0300

    Requisicoes dos tres sites

--- js.js
+++ js.js
@@ -1,13 +1,14 @@
 function montarPagina(pagina){
     var parser = new DOMParser();
     var documento = parser.parseFromString(pagina, "text/html");
-    documento.querySelectorAll(".slick-slide").forEach(div => {
-        var li = div.children;
-        // Array.from(as).forEach(function(a){
-        //     var noticia = a.children;
-        //     Array.from(noticia).forEach(x => document.body.appendChild(x));
+    documento.querySelectorAll(".slick-list draggable")
+    .forEach(div => {
+        var bbb = div.children;
+        console.log(bbb);
+        // Array.from(aaa).forEach(x => {
+        //     var noticia = x.children;
+        //     Array.from(noticia).forEach(y => document.body.appendChild(y));
         // });
-        console.log(li);
         // var div2 = document.createElement("div");
         // div2.innerHTML = p;
         // document.body.appendChild(div2);
@@ -22,7 +23,7 @@ function NerdonautasReq() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             montarPagina(this.responseText);
-            console.log("OK");
+            console.log("OK nerdonautas");
         }
         else{
             console.log("erro");
@@ -32,14 +33,36 @@ function NerdonautasReq() {
     xhttp.send();
 }
 
-function teste(){
-    NerdonautasReq();
+function StudioGeekReq() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://www.studiogeek.com.br/", true);
+    
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            montarPagina(this.responseText);
+            console.log("OK studio");
+        }
+        else{
+            console.log("erro");
+        }
+    };
+    
+    xhttp.send();
 }
 
-function criarEvento(){
-    document.querySelector("button").addEventListener("click",function(){
-        teste();
-    })
+function ComicStoreReq() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://www.comicstore.com.br/", true);
+    
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            montarPagina(this.responseText);
+            console.log("OK comic");
+        }
+        else{
+            console.log("erro");
+        }
+    };
+    
+    xhttp.send();
 }
-
-window.onload = criarEvento;
