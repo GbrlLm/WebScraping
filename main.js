9ce408f8bd6b4e1ed3ee7377e76d2ab8f951ce0a
gabriel <gabriel.lima@modalgr.com.br>
2019-05-29 17:32:59 -0300

    Atualizações de estrutura HTML e passagem de param no js

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
 function getUrlObject(){
     return [ 
-        {url:"https://www.comicstore.com.br/", callback: montarPaginaComic},
-        {url: "https://www.studiogeek.com.br/", callback: montarPaginasGeekNerd}, 
-        {url: "https://www.imaginarionerd.com.br/", callback: montarPaginasGeekNerd}
+        {url:"https://www.comicstore.com.br/", callback: montarPaginaComic, title: "comicstore"},
+        {url: "https://www.studiogeek.com.br/", callback: montarPaginasGeekNerd, title: "studiogeek"}, 
+        {url: "https://www.imaginarionerd.com.br/", callback: montarPaginasGeekNerd, title: "imaginarionerd"}
     ]
 }
 
 window.onload = async function() {
     getUrlObject().forEach(async item => {
-        console.log("antes result", item.url)
-        await makeRequest("GET", item.url, item.callback);
-        console.log("result ", item.url);
+        console.log("antes result")
+        await makeRequest("GET", item.url, item.callback, item.title);
     });
     console.log("carrosel");
     carrossel();
 
 };
 
-function makeRequest(method, url, pagina) {
+function makeRequest(method, url, pagina, nomePag) {
     new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
         xhr.open(method, url, false);
         xhr.onreadystatechange = async function () {
             if (this.status >= 200 && this.status < 300) {
                 console.log("resolve makeReq");
-                console.log("pagina",pagina);
-                pagina(xhr.response);
+                pagina(xhr.response, nomePag);
+
                 resolve({resultado: "OK"});
             } else {
                 reject({
@@ -44,12 +43,14 @@ function makeRequest(method, url, pagina) {
     }); 
 }
 
-function montarPaginasGeekNerd(pagina){
+function montarPaginasGeekNerd(pagina, nomePag){
     var parser = new DOMParser();
     var documento = parser.parseFromString(pagina, "text/html");
 
-    let  elementos = documento.querySelectorAll(".listagem-linha .span4 .listagem-item");
+    let elementos = documento.querySelectorAll(".listagem-linha .span4 .listagem-item");
+    let logo = documento.querySelector(".span3 .logo a");
     let div = document.createElement('div');
+    let divPagina = document.querySelector(`.${nomePag}`);
 
     elementos.forEach(item => {
         item.children[3].style.display = "none";
@@ -57,78 +58,40 @@ function montarPaginasGeekNerd(pagina){
         item.children[5].style.display = "none";
         div.setAttribute("class", "carousel");
         div.appendChild(item);
-        document.body.appendChild(div);
+        divPagina.appendChild(logo);
+        divPagina.appendChild(div);
     });
     console.log("pagina montada");
 }
 
-function montarPaginaComic(pagina){
+function montarPaginaComic(pagina, nomePag){
     var parser = new DOMParser();
     var documento = parser.parseFromString(pagina, "text/html");
+    
 
     let  elementos = documento.querySelectorAll(".listagem-linha .span3 .listagem-item");
+    let logo = documento.querySelector(".span3 .logo a");
     let div = document.createElement('div');
+    let divPagina = document.querySelector(`.${nomePag}`);
 
     elementos.forEach(item => {
         item.children[3].style.display = "none";
         div.setAttribute("class", "carousel");
         div.appendChild(item);
-        document.body.appendChild(div);
+        divPagina.appendChild(logo);
+        divPagina.appendChild(div);
     });
     console.log("pagina montada");
 }
 
 function carrossel(){
-    
-        document.querySelectorAll(".carousel");
-
-                    $('.carousel').slick({
-                    dots: true,
-                    infinite: true,
-                    speed: 500,
-                    slidesToShow: 4,
-                    slidesToScroll: 4,
-                  });
+    document.querySelectorAll(".carousel");
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
     console.log("fim do carrosel");
 }
-
-
-    
-// var classes = document.querySelectorAll(".carousel");
-// classes.forEach( item => {
-//     $(`.${item.className}`).slick({
-//         dots: true,
-//         infinite: true,
-//         speed: 500,
-//         slidesToShow: 4,
-//         slidesToScroll: 4,
-//     });
-// });
-
-// $(aaa.className).each(function(){
-//     var slickInduvidual = $(this);
-//     slickInduvidual.slick({
-//         dots: true,
-//         infinite: true,
-//         speed: 500,
-//         slidesToShow: 4,
-//         slidesToScroll: 4,
-//     });
-// })
-
-// aaa.forEach(div => {div.slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 4,
-//     slidesToScroll: 4,
-//   });
-// })
-
-// $('.carousel').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 4,
-//     slidesToScroll: 4,
-//   });
